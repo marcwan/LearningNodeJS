5c92c8b495458377de7b3aeb7e63e9ddac541927
陶小陶 <leoorpio@live.com>
2016-06-23 11:18:04 +0800

    modify
    results[results.length] = arr1[j]; It is not arr1[j] here.If arr1.length is less than j.something is wrong.

--- Chapter03/09_expensive.js
+++ Chapter03/09_expensive.js
@@ -5,7 +5,7 @@ function compute_intersection(arr1, arr2) {
     for (var i = 0 ; i < arr1.length; i++) {
         for (var j = 0; j < arr2.length; j++) {
             if (arr2[j] == arr1[i]) {
-                results[results.length] = arr1[j];
+                results[results.length] = arr2[j];
                 break;
             }
         }
